2dab98bd5e2f47d4fb1c28230ab22de498dd62c3
hannah-ric <hannah.ricci@travelandleisure.com>
2025-06-04 18:11:09 -0400

    Fix package.json and clean up source

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,14 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 export default [
   { ignores: ['dist'] },
   {
+    files: ['index.js', 'tests/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: { ...globals.node, ...globals.jest },
+      sourceType: 'commonjs',
+    },
+  },
+  {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
